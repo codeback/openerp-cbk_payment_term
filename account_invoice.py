c2c2a144e19aab8ed19ed1d24dc0e232f243d5c4
root <root@codeback.es>
2013-11-05 09:25:45 +0100

    Añadido funcionalidad para sacar el pago del vencimiento más cercano

--- account_invoice.py
+++ account_invoice.py
@@ -29,23 +29,34 @@ class account_invoice(osv.osv):
     _name = "account.invoice"
     _inherit = "account.invoice"
     
-    def _get_next_date_due(self, cr, uid, ids, field_names, arg, context=None):
+    def _get_next_payment(self, cr, uid, ids, field_names, arg, context=None):
 
         vals={}
         for invoice in self.browse(cr, uid, ids):            
             vals[invoice.id] = invoice.date_due
+            next_payment = invoice.residual
             if invoice.move_id:
                 for line in invoice.move_id.line_id:
                     if line.date_maturity and not line.reconcile:
                         if vals[invoice.id] > line.date_maturity:
                             vals[invoice.id] = line.date_maturity
-            value = {'s_next_date_due': vals[invoice.id]}
+                            
+                            if invoice.type == "out_invoice" or invoice.type == "in_refund":
+                                next_payment = line.debit
+                            else:
+                                if invoice.type == "in_invoice" or invoice.type == "out_refund":
+                                    next_payment = line.credit
+            value = {
+                's_next_date_due': vals[invoice.id], 
+                'next_payment': next_payment,
+            }
             self.write(cr, uid, [invoice.id], value, context=context)
         return  vals
 
     _columns = {        
-        'next_date_due' : fields.function(_get_next_date_due, string='Next Due Date', type='date'),
+        'next_date_due' : fields.function(_get_next_payment, string='Next Due Date', type='date'),
         's_next_date_due': fields.date(string='Next Due Date', readonly=True), # Necesaria para poder agrupar en el buscador (en campos function no se puede)
+        'next_payment' : fields.float(string='Next Payment', readonly=True),
     }  
 
     
